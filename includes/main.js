6e7d7ef2a94771fa23b8e4fceb30375fd203ca0a
Sagnik Modak <sagnikmodak1118@gmail.com>
2018-02-06 17:29:38 +0530

    Minor Fixes

--- includes/main.js
+++ includes/main.js
@@ -222,7 +222,7 @@ function openNav() {
 				initPageView();
 			}else{
 				$("html,body").css({"min-height":"100%"});
-				$(".content-container").css({'position':'initial','overflow-x':'hidden'});
+				$(".content-container").css({'position':'relative','overflow-x':'hidden'});
 			}
 		});
 		
@@ -252,4 +252,17 @@ function openNav() {
 		
 		var options = {"particles":{"number":{"value":99,"density":{"enable":true,"value_area":552.4033491425909}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#ffffff"},"polygon":{"nb_sides":3},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":2,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1.5782952832645452,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"repulse"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":6,"speed":2},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":false};
 		particlesJS("particle", options);
+		
+		$(window).scroll(function (){
+			var offset = 25;
+			var limit = Math.max( document.body.scrollHeight, document.body.offsetHeight, 
+						   document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
+			var alpha = 0;
+			if($(window).scrollTop() > 25){
+				alpha = $(window).scrollTop()*10/limit;
+				$('#mobileNav').css({'background':'rgba(18, 29, 49, '+alpha+')'});
+			}else{
+				$('#mobileNav').css({'background':'transparent'});
+			}
+		  });
 	});
