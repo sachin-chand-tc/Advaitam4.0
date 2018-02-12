e6bed1db10f1638d85a93757b2b447867de51267
crownedlake <schand1997@gmail.com>
2018-02-12 17:37:06 +0530

    modified

--- includes/main.js
+++ includes/main.js
@@ -149,7 +149,7 @@ function openNav() {
 			height: 0
 		},
 		{
-			height: 10
+			height: 15
 		})
 		.to($overview, 8, {autoAlpha: 0}, '+=0.3')
 		.from($quiz, 8, {autoAlpha: 0}, '-=0.3', '0')
@@ -225,29 +225,6 @@ function openNav() {
 				$(".content-container").css({'position':'relative','overflow-x':'hidden'});
 			}
 		});
-		
-		/* Fix for fast scrolling on laptop or desktop with mouse */
-		if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
-		window.onmousewheel = document.onmousewheel = wheel;
-
-		function wheel(event) {
-			var delta = 0;
-			if (event.wheelDelta) delta = event.wheelDelta / 120;
-			else if (event.detail) delta = -event.detail / 3;
-
-			handle(delta);
-			if (event.preventDefault) event.preventDefault();
-			event.returnValue = false;
-		}
-
-		function handle(delta) {
-			var time = 500;
-			var distance = 700;
-			
-			$('html, body').stop().animate({
-				scrollTop: $(window).scrollTop() - (distance * delta)
-			}, time );
-		}
 		
 		
 		var options = {"particles":{"number":{"value":99,"density":{"enable":true,"value_area":552.4033491425909}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#ffffff"},"polygon":{"nb_sides":3},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":2,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1.5782952832645452,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"repulse"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":6,"speed":2},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":false};
